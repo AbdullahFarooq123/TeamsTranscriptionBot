1f6f08e751b78dd40ff6022c3da056c11d235663
KHAN\Dev <bilal@reborncode.com>
2023-11-17 15:55:01 +0500

    Add project files.

---
+++ MeetingTranscription/Models/Transcripts.cs
@@ -0,0 +1,17 @@
+﻿namespace MeetingTranscription.Models
+{
+    using System;
+
+    public class Transcripts
+    {
+        /// <summary>
+        /// Id of transcript.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Created date of trancript.
+        /// </summary>
+        public DateTime CreatedDateTime { get; set; }
+    }
+}
